64bbcdb788aca1a7eb9b0024daa1bf112273fb25
VictorCyprien <victor.cyprien@limayrac.fr>
2023-05-06 10:56:19 +0200

    Add tests

--- tests/api/barbecues/put/test_api_put_barbecue.py
+++ tests/api/barbecues/put/test_api_put_barbecue.py
@@ -98,3 +98,31 @@ def test_barbecue_error_during_update(client: Flask, victor: User, toulouse: Bar
     }
 
     mock_save_barbecue_document.assert_called()
+
+
+
+def test_barbecue_update_not_found(client: Flask, victor: User):
+    data_login = {
+        "email": victor.email,
+        "password": "beedemo"
+    }
+
+    res = client.post("/api/auth/login", json=data_login)
+    token = res.json["token"]
+    assert res.status_code == 201
+
+    data_put = {
+        "name": "Mon autre barbuc à Paris",
+        "place": "Toujours à Paris",
+        "date": "2023-05-15 12:45:00"
+    }
+
+    res = client.put("/api/barbecues/123", json=data_put, headers={'Authorization': f'Bearer {token}'})
+    assert res.status_code == 404
+    data = res.json
+    print(data)
+    assert data == {
+        'code': 404, 
+        'message': 'Barbecue #123 not found !', 
+        'status': 'Not Found'
+    }
